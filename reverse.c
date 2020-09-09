470d3dcb90537032c49157be46ba49ffef72b3c8
Isaiah Gathala <55990958+isaiahgman@users.noreply.github.com>
2020-09-09 10:02:40 -0400

    Update reverse.c

--- reverse.c
+++ reverse.c
@@ -1,19 +1,17 @@
 #include <stdio.h>
 
 int main(){
-	
-	int rem;
-	int num;
-	int reversedNum = 0;
-
+	//intialize variables
+	int rem, num, reversedNum = 0;
+	//take user input
 	printf("Enter a number to be reversed: ");
 	scanf("%d", &num);
-
+	//cycle through and create reversed number
 	while (num >= 1){
 		rem = num % 10;
 		reversedNum = (reversedNum * 10) + rem;
 		num = num / 10;
 	} 
-
+	//output
 	printf("This is the reversed number: %d\n", reversedNum);
 }
